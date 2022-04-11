c2ff6ed4bb9f5c6afc0b6298c6084b60f9d7f44e
dell <david.cohn.82@gmail.com>
2022-04-12 01:50:11 +0300

    Added create_imgui_cmake.sh

---
+++ create_imgui_cmake.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/Pesc0/imgui-cmake --recurse-submodules
+cd imgui-cmake
+git submodule foreach git pull origin HEAD
+mkdir build && cd build
+cmake -D BUILD_EXAMPLES=ON ..
+cmake --build . -j4
+./example_sdl_opengl3
+
