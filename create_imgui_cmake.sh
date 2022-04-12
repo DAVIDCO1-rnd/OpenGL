9ef1077c460343eca3cca7790d8f6c8530d177f6
David <davidco1-rnd@hotmail.com>
2022-04-12 11:08:04 +0300

    updated create_imgui_cmake.sh to run in Debug

--- create_imgui_cmake.sh
+++ create_imgui_cmake.sh
@@ -2,7 +2,7 @@ git clone https://github.com/Pesc0/imgui-cmake --recurse-submodules
 cd imgui-cmake
 git submodule foreach git pull origin HEAD
 mkdir build && cd build
-cmake -D BUILD_EXAMPLES=ON ..
+cmake -D BUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build . -j4
 ./example_sdl_opengl3
 
