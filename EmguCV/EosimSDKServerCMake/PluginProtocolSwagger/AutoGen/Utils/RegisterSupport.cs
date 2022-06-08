aef016d2ef79275ae631bf04c50e4c4fa17ddaf2
David Cohn <DAVIDCO1@rnd-hub.com>
2022-06-09 00:52:36 +0300

    Added folder EosimSDKServerCMake

---
+++ EmguCV/EosimSDKServerCMake/PluginProtocolSwagger/AutoGen/Utils/RegisterSupport.cs
@@ -0,0 +1,12 @@
+﻿using EOSimU.API.AutoGen.v1alpha3.Models;
+using EOSimU.API.AutoGen.v1alpha3.Modules;
+using EOSimU.API.AutoGen.v1alpha3.Utils;
+using Nancy.TinyIoc;
+
+namespace Implementation
+{
+    public static class RegisterSupport
+    {
+        public static Scen
+    }
+}
