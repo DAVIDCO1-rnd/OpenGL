ef1f578e8ff59cb1b836587a34fb0002c843d997
David Cohn <davidco1@rnd-hub.com>
2022-06-30 16:35:23 +0300

    updated python code

--- EmguCV/ClientAndServer/Client_python/src/example.py
+++ EmguCV/ClientAndServer/Client_python/src/example.py
@@ -1,9 +1,24 @@
 from __future__ import print_function
 
+import numpy as np
+import cv2
 import time
 import openapi_client
 from openapi_client.rest import ApiException
 from pprint import pprint
+
+def convertPolygonsListToTuple(polygons):
+    polygonsList = []
+    for polygon in polygons:
+        numOfPoints = len(polygon.vertices)
+        polygonNumpy = np.zeros((numOfPoints, 1, 2), dtype=int)
+        for i in range(0, numOfPoints):
+            point = polygon.vertices[i]
+            polygonNumpy[i, 0, 0] = point.x
+            polygonNumpy[i, 0, 1] = point.y
+        polygonsList.append(polygonNumpy)
+    polygonsTuple = tuple(polygonsList)
+    return polygonsTuple
 
 # Defining the host is optional and defaults to http://localhost:8080/v1alpha3
 # See configuration.py for a list of all supported configuration parameters.
@@ -32,16 +47,23 @@ except ApiException as e:
 try:
     # Returns a list of polygons given a target location (latitude, longtitude, altitude) and a height above the target. Meaning the parameter is an array of 4 doubles (latitude, longtitude, altitude, height)
     polygons = api_instance.scene_polygons(camera_x, camera_y, camera_z, plate_height_above_target)
-    numOfPolygons = len(polygons)
-    for polygon in polygons:
-        numOfPoints = len(polygon.vertices)
-        for i in range(0, numOfPoints):
-            point = polygon.vertices[i]
-            x = point.x
-            y = point.y
+    contours1 = convertPolygonsListToTuple(polygons)
+    filename = '../../network_folder/screenShot.bmp'
+    color_image = cv2.imread(filename, cv2.IMREAD_COLOR)
+    image_with_contours = color_image.copy()
 
-            pass
+    grayscale_image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    thresh = 127
+    binary_image = cv2.threshold(grayscale_image, thresh, 255, cv2.THRESH_BINARY)[1]
+    contours, hierarchy = cv2.findContours(binary_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(image=image_with_contours, contours=contours1, contourIdx=-1, color=(0, 255, 0), thickness=2,
+                     lineType=cv2.LINE_AA)
+    cv2.imshow("image_with_contours", image_with_contours)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    #pprint(polygons)
+    pass
+
+    #pprint(contours)
 except ApiException as e:
     print("Exception when calling PolygonsLosApi->scene_polygons: %s\n" % e)
--- EmguCV/ClientAndServer/Client_python/src/my_script.py
+++ EmguCV/ClientAndServer/Client_python/src/my_script.py
@@ -1,7 +1,7 @@
 import cv2
 
 
-filename = 'C:/temp/OpenGL/EmguCV/ClientAndServer/images/screenShot.bmp'
+filename = '../../images/screenShot.bmp'
 color_image = cv2.imread(filename, cv2.IMREAD_COLOR)
 grayscale_image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 thresh = 127
@@ -14,28 +14,6 @@ cv2.drawContours(image=image_with_contours, contours=contours, contourIdx=-1, co
 tt = contours[1]
 print(tt)
 
-# cv2.imshow("image_with_contours", image_with_contours)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-
-# import cv2
-#
-# filename = 'C:/temp/OpenGL/EmguCV/ClientAndServer/images/screenShot.bmp'
-# image = cv2.imread(filename)
-#
-# # B, G, R channel splitting
-# blue, green, red = cv2.split(image)
-#
-# # detect contours using blue channel and without thresholding
-# contours1, hierarchy1 = cv2.findContours(image=blue, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
-#
-# # draw contours on the original image
-# image_contour_blue = image.copy()
-# cv2.drawContours(image=image_contour_blue, contours=contours1, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
-# # see the results
-# cv2.imshow('Contour detection using blue channels only', image_contour_blue)
-# cv2.waitKey(0)
-# cv2.imwrite('blue_channel.jpg', image_contour_blue)
-# cv2.destroyAllWindows()
-
+cv2.imshow("image_with_contours", image_with_contours)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
